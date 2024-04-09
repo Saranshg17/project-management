181537106329be5bf05a0213f17517e6ce2aa239
Saransh Goyal <saranshgoyal@Saranshs-MacBook-Air.local>
2024-04-09 15:08:16 +0530

    Changed role from text to dropdown

--- frontend/src/SigupPage.js
+++ frontend/src/SigupPage.js
@@ -1,10 +1,10 @@
 // SignupPage.js
 import React, { useState } from 'react';
-import axios from 'axios';
+import axios from 'axios'; // Import axios for making HTTP requests
 import './styles.css'; // Import your CSS file for styling
 
 const SignupPage = () => {
-  const [signupData, setSignupData] = useState({ email: '', password: '', confirmPassword: '', role: '' });
+  const [signupData, setSignupData] = useState({ email: '', password: '', confirmPassword: '', role: 'Standard User' });
   const [passwordMatchError, setPasswordMatchError] = useState('');
   const [registrationError, setRegistrationError] = useState('');
 
@@ -19,7 +19,7 @@ const SignupPage = () => {
       const response = await axios.post('http://localhost:8000/api/v1/users/register', signupData);
       console.log('Registration successful:', response.data);
       // Reset form and state after successful registration
-      setSignupData({ email: '', password: '', confirmPassword: '', role: '' });
+      setSignupData({ email: '', password: '', confirmPassword: '', role: 'Standard User' });
       setPasswordMatchError('');
       setRegistrationError('');
       // Redirect user to login page or show success message
@@ -27,6 +27,10 @@ const SignupPage = () => {
       console.error('Registration failed:', error.response.data);
       setRegistrationError('Registration failed. Please try again.'); // Display error message to user
     }
+  };
+
+  const handleRoleChange = (e) => {
+    setSignupData({ ...signupData, role: e.target.value }); // Update role state with the selected value
   };
 
   return (
@@ -38,13 +42,6 @@ const SignupPage = () => {
           placeholder="Email"
           value={signupData.email}
           onChange={(e) => setSignupData({ ...signupData, email: e.target.value })}
-          required
-        />
-        <input
-          type="role"
-          placeholder="Role"
-          value={signupData.role}
-          onChange={(e) => setSignupData({ ...signupData, role: e.target.value })}
           required
         />
         <input
@@ -62,6 +59,11 @@ const SignupPage = () => {
           required
         />
         {passwordMatchError && <p className="error">{passwordMatchError}</p>}
+        <select id="role" value={signupData.role} onChange={handleRoleChange}>
+          <option value="Standard User">Standard User</option>
+          <option value="Admin Role">Admin Role</option>
+        </select>
+        {registrationError && <p className="error">{registrationError}</p>}
         <button type="submit">Signup</button>
       </form>
     </div>
@@ -69,3 +71,4 @@ const SignupPage = () => {
 };
 
 export default SignupPage;
+
